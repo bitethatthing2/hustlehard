be0f34ab2090f2b9d6bea738d42678fca4b65ed4
michael kahler <gthabarber1@gmail.com>
2025-03-14 23:49:15 -0700

    Remove development features from notification-icon.js

--- public/notification-icon.js
+++ public/notification-icon.js
@@ -1,1 +1,13 @@
-// TEST EDIT - CLEARLY DIFFERENT CONTENT
+﻿// Android notification icon helper
+function getNotificationIconPath() {
+  const userAgent = navigator.userAgent.toLowerCase();
+  const isAndroid = userAgent.indexOf('android') > -1;
+  
+  if (isAndroid) {
+    return '/only_these/android/android-launchericon-96-96.png';
+  }
+  
+  return null;
+}
+
+window.getNotificationIconPath = getNotificationIconPath;
