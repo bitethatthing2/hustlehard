76e94425076ffca7f6e349505596010ad64da563
michael kahler <gthabarber1@gmail.com>
2025-03-14 23:56:50 -0700

    Remove unnecessary test and simple notification files

--- public/notification-icon.js
+++ public/notification-icon.js
@@ -1,13 +1,21 @@
 ﻿// Android notification icon helper
+// This script helps with setting up the small icon for Android notifications
+
+// Function to get the appropriate notification icon path based on device
 function getNotificationIconPath() {
-  const userAgent = navigator.userAgent.toLowerCase();
-  const isAndroid = userAgent.indexOf('android') > -1;
-  
-  if (isAndroid) {
+  // For Android, use a PNG with transparency
+  if (/Android/.test(navigator.userAgent)) {
     return '/only_these/android/android-launchericon-96-96.png';
   }
   
-  return null;
+  // For iOS, use Apple icon
+  if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+    return '/only_these/ios/apple-icon-180x180.png';
+  }
+  
+  // Default icon for other platforms
+  return '/only_these/windows11/SmallTile.scale-400.png';
 }
 
+// Make function available globally
 window.getNotificationIconPath = getNotificationIconPath;
